0d9120dd9a555a0410eedeb41472ba4d985460d0
Arsenii <f29535@bk.ru>
2022-10-07 17:39:01 +0300

    Update!
    Implemented 2 buttons for refactoring and deleting post. Also executed dynamic urls for articles

--- prepare/news/urls.py
+++ prepare/news/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.news_home, name='news_home'),
-    path('create', views.create, name='create')
+    path('create', views.create, name='create'),
+    path('<int:pk>', views.NewsDetailViews.as_view(), name='news_detail'),
+    path('<int:pk>/update', views.NewsUpdateViews.as_view(), name='news_update'),
+    path('<int:pk>/delete', views.NewsDeleteViews.as_view(), name='news_delete')
 ]
--- prepare/news/views.py
+++ prepare/news/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
+from django.views.generic import DetailView, UpdateView, DeleteView
 
 
 def news_home(request):
     news = Articles.objects.order_by('-date')
     return render(request, 'news/news_home.html', {'news': news})
+
+
+class NewsDetailViews(DetailView):
+    model = Articles
+    template_name = 'news/details_view.html'
+    context_object_name = 'article'
+
+
+class NewsUpdateViews(UpdateView):
+    model = Articles
+    template_name = 'news/create.html'
+
+    #fields = ['title', 'anons', 'full_text', 'date']
+
+    form_class = ArticlesForm
+
+
+class NewsDeleteViews(DeleteView ):
+    model = Articles
+    success_url = '/news/'
+    template_name = 'news/news_delete.html'
 
 
 def create(request):
